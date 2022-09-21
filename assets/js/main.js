9bfeb3d9a92c86880eed6183195c2c3d40edc296
kyoto430 <nateriver345@gmail.com>
2022-09-21 13:25:21 +0300

    added home section, images

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,24 @@ navMenu.addEventListener('click', ({ target }) => {
     navMenu.classList.remove('show-menu')
   }
 })
+
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slide')
+
+  slides[activeSlide].classList.add('active')
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('active')
+    })
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active')
+    })
+  }
+}
+
+slidesPlugin()
