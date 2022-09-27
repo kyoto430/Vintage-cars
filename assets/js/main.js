1e031c1d8e34285a707d1a5ca6e557b6b69bc45c
kyoto430 <nateriver345@gmail.com>
2022-09-27 08:56:33 +0300

    added preview section, contact section, footer, content images and animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,4 +44,22 @@ function slidesPlugin(activeSlide = 0) {
   }
 }
 
-slidesPlugin()
+slidesPlugin(2)
+
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show')
+    }
+  })
+}
+
+let options = {
+  threshold: [0.5],
+}
+let observer = new IntersectionObserver(onEntry, options)
+let elements = document.querySelectorAll('.element-animation')
+
+for (let elm of elements) {
+  observer.observe(elm)
+}
